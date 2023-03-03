abe6511969adf64b1c544a09303a2930445d8c5f
Anyiekenwa <francobenny922@gmail.com>
2023-03-03 05:36:56 -0800

    Hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the string with proper grammer, but the outcome is
+ * a piece of art to the console.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
