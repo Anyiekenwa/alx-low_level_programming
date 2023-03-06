fb6391b50488cf4f775ea673e71ace83739fee37
Anyiekenwa <francobenny922@gmail.com>
2023-03-06 04:58:49 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet in lowercae followed by newline
+ * Repeats 10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 26; j++)
+		{
+			_putchar('a' + j);
+		}
+		_putchar('\n');
+	}
+}
